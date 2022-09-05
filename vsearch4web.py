e34a5315989ea8a5d34111cc8056a4f8f533ebef
Jakub Turner <kuba.turner@gmail.com>
2022-09-05 09:34:54 +0200

    add database

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-
+import mysql.connector
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep=' | ')
+    # with open('vsearch.log', 'a') as log:
+    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep=' | ')
+    dbconfig = {
+        'host': '127.0.0.1',
+        'user': 'vsearch',
+        'password': 'vsearchpasswd',
+        'database': 'vsearchLogDB',
+    }
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+
+    _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s)"""
+
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'], req.remote_addr, req.user_agent.string,
+                          res,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 @app.route('/search4', methods=['POST'])
@@ -35,7 +54,7 @@ def view_the_log() -> "html":
             for item in line.split('|'):
                 contents[-1].append(escape(item))
     titles = ('Form Data', 'Remote_addr', 'User_Agent', 'Results')
-    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
+    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents, )
 
 
 if __name__ == '__main__':
