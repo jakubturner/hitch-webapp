f1920942bb25fc353d386e562aa91c4e86f2eda9
Jakub Turner <kuba.turner@gmail.com>
2022-09-12 20:29:25 +0200

    add viewlog fc to view database

--- vsearch4web.py
+++ vsearch4web.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['dbconfig'] = {
     'host': '127.0.0.1',
     'user': 'vsearch',
-    'password': 'searchpasswd',
+    'password': 'vsearchpasswd',
     'database': 'vsearchLogDB',
 }
 
@@ -43,13 +43,20 @@ def render_entry() -> "html":
 
 @app.route('/viewlog')
 def view_the_log() -> "html":
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_Agent', 'Results')
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+
+    # contents = []
+    # with open('vsearch.log') as log:
+    #     for line in log:
+    #         contents.append([])
+    #         for item in line.split('|'):
+    #             contents[-1].append(escape(item))
+    # titles = ('Form Data', 'Remote_addr', 'User_Agent', 'Results')
+
     return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents, )
 
 
